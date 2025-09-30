fa23cc7aef1070500885e2f8304f0b0d35845f92
ssacko <#s19LU89>
2025-09-30 15:51:50 +0000

    Ajout des attributs referential et typeLocation

--- src/actions.js
+++ src/actions.js
@@ -29,7 +29,7 @@ const LANGUAGE_FULL_PROJECTION = () => ["name", "code"];
 
 const MODULEPERMISSION_FULL_PROJECTION = () => ["modulePermsList{moduleName, permissions{permsName, permsValue}}"];
 
-const CUSTOM_FILTER_FULL_PROJECTION = () => ["type", "code", "possibleFilters {field, filter, type}"];
+const CUSTOM_FILTER_FULL_PROJECTION = () => ["type", "code", "possibleFilters {field, filter, type, referential, typeLocation}"];
 
 export function fetchCustomFilter(params) {
   const payload = formatQuery("customFilters", params, CUSTOM_FILTER_FULL_PROJECTION());
@@ -52,7 +52,7 @@ function getCsrfToken() {
 
   const cookies = document.cookie;
   const cookieArray = cookies.split('; ');
-  
+
   const csrfCookie = cookieArray.find(cookie => cookie.startsWith(CSRF_TOKEN_NAME));
   return csrfCookie?.split('=')[1] ?? CSRF_NOT_FOUND;
 }
@@ -280,7 +280,7 @@ export function login(credentials) {
           dispatch(authError({ message: errorMessage }));
           return { loginStatus: "CORE_AUTH_ERR", message: errorMessage };
         }
-        
+
         const jwtToken = response.payload.data.tokenAuth.token;
         const csrfResponse = await dispatch(fetchCsrfToken(jwtToken));
         const csrfToken = csrfResponse?.payload?.data?.getCsrfToken?.csrfToken;
